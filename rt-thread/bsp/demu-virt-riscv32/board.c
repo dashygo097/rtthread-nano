b1377962281ddfb37a9543df9934ef0f91d12a17
dashygo097 <dashygo097@github.com>
2026-04-20 21:41:13 +0800

    [demu] checkpoint

--- rt-thread/bsp/demu-virt-riscv32/board.c
+++ rt-thread/bsp/demu-virt-riscv32/board.c
@@ -1,56 +1,54 @@
+#include <rtconfig.h>
 #include <rthw.h>
 #include <rtthread.h>
 
-#define UART_TXD (*(volatile unsigned int *)0x10000004u)
-#define CLINT_MSIP (*(volatile unsigned int *)0x20000000u)
+#define UART_TXD_REG (*(volatile unsigned char *)0x10000004u)
 #define CLINT_MTIMECMP_LO (*(volatile unsigned int *)0x20004000u)
 #define CLINT_MTIMECMP_HI (*(volatile unsigned int *)0x20004004u)
 #define CLINT_MTIME_LO (*(volatile unsigned int *)0x2000BFF8u)
 #define CLINT_MTIME_HI (*(volatile unsigned int *)0x2000BFFCu)
 
-#ifndef TICK_CYCLES
-#define TICK_CYCLES 1000u
-#endif
-
-extern unsigned int _ebss;
-extern unsigned int __stack;
+#define TICK_CYCLES 500000u
 
 extern void IRQ_Handler(void);
 
+void update_timer(void) {
+  unsigned int lo = CLINT_MTIME_LO;
+  unsigned int hi = CLINT_MTIME_HI;
+  unsigned long long current_mtime = ((unsigned long long)hi << 32) | lo;
+  unsigned long long next_mtime = current_mtime + TICK_CYCLES;
+
+  CLINT_MTIMECMP_HI = 0xFFFFFFFFu;
+  CLINT_MTIMECMP_LO = (unsigned int)(next_mtime & 0xFFFFFFFFu);
+  CLINT_MTIMECMP_HI = (unsigned int)(next_mtime >> 32);
+}
+
 void SystemIrqHandler(rt_uint32_t mcause) {
   if (mcause == 0x80000007u) {
-    CLINT_MTIMECMP_HI = 0xFFFFFFFFu;
-    CLINT_MTIMECMP_LO = CLINT_MTIME_LO + TICK_CYCLES;
-    CLINT_MTIMECMP_HI = 0u;
-
+    update_timer();
     rt_interrupt_enter();
     rt_tick_increase();
     rt_interrupt_leave();
-  } else if (mcause == 0x80000003u) {
-    CLINT_MSIP = 0u;
+  } else {
+    while (1)
+      ;
   }
 }
 
 void rt_hw_board_init(void) {
-#if defined(RT_USING_USER_MAIN) && defined(RT_USING_HEAP)
-  rt_uint8_t *heap_begin = (rt_uint8_t *)&_ebss;
-  rt_uint8_t *heap_end = (rt_uint8_t *)((unsigned int)&__stack - 8192u);
+  void *heap_begin = (void *)0x80041000u;
+  void *heap_end = (void *)0x80078000u;
+
   rt_system_heap_init(heap_begin, heap_end);
-#endif
 
-  CLINT_MTIMECMP_HI = 0xFFFFFFFFu;
-  CLINT_MTIMECMP_LO = CLINT_MTIME_LO + TICK_CYCLES;
-  CLINT_MTIMECMP_HI = 0u;
+  asm volatile("csrw mtvec, %0" ::"r"(IRQ_Handler));
 
-  __asm__ volatile("csrw mtvec, %0" ::"r"(IRQ_Handler) : "memory");
+  update_timer();
 
-  __asm__ volatile("li   t0, 0x80  \n"
-                   "csrw mie, t0   \n" ::
-                       : "t0");
-
-  __asm__ volatile("li   t0, 0x8       \n"
-                   "csrs mstatus, t0   \n" ::
-                       : "t0");
+  /* Enable Machine Timer Interrupt */
+  asm volatile("li t0, 128 \n"
+               "csrs mie, t0 \n" ::
+                   : "t0");
 
 #ifdef RT_USING_COMPONENTS_INIT
   rt_components_board_init();
@@ -61,8 +59,8 @@ void rt_hw_board_init(void) {
 void rt_hw_console_output(const char *str) {
   while (*str) {
     if (*str == '\n')
-      UART_TXD = (unsigned int)'\r';
-    UART_TXD = (unsigned int)(unsigned char)*str++;
+      UART_TXD_REG = '\r';
+    UART_TXD_REG = *str++;
   }
 }
 #endif
